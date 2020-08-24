0af5cd106eec56772f95c4022a77048385316b3b
Vivian Imbriotis <viviani@utas.edu.au>
2020-08-24 18:02:41 +1000

    utf8 decoding fix

--- RScripts/r_interface.py
+++ RScripts/r_interface.py
@@ -8,19 +8,25 @@ import subprocess
 from os import path
 from accdatatools.RScripts import __path__ as r_script_root
 
-RSCRIPT_INTERPRETER = "C:\\Program Files\\R\\R-4.0.1\\bin\\Rscript.exe"
+RSCRIPT_INTERPRETER = "c:\\Program Files\\R\\R-3.6.0\\bin\\Rscript.exe"
 
 
 def execute_r_script(r_script_name, *args):
-    path_to_r_script = path.join(r_script_root._path[0],
+    try:
+        r_script_root_path = r_script_root._path[0]
+    except AttributeError:
+        r_script_root_path = r_script_root[0]
+    path_to_r_script = path.join(r_script_root_path,
                                  r_script_name)
     command = [RSCRIPT_INTERPRETER, path_to_r_script]
     if args:
         command.extend(args)
+    print(command)
     result = subprocess.check_output(command)
-    return result.decode("utf-8")
+    return result.replace(b"\r\n",b"\n").decode("utf-8")
 
 if __name__=="__main__":
     a = execute_r_script(
-        "example_r_script.r")
+        "pupil_size_mixed_linear_model.R")
     print(a)
+    
