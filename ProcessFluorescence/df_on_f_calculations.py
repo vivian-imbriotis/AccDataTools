115a3892c9c6ee47d87b4e618e6b8970de7e8312
Vivian Imbriotis <viviani@utas.edu.au>
2020-08-31 09:48:45 +1000

    GD attempt (not working)

--- ProcessFluorescence/df_on_f_calculations.py
+++ ProcessFluorescence/df_on_f_calculations.py
@@ -27,3 +27,112 @@ def get_df_on_f0(F,F0=None):
     else:
         F0 = get_smoothed_running_minimum(F)
         return get_df_on_f0(F,F0)
+    
+    
+
+
+def underline_cost_func(params, x, y):
+    guess = params[0] + params[1] * x
+    residuals = y - guess #data points above line are positive
+    upper_cost = np.sum(residuals[residuals > 0]**2 / np.sum(residuals > 0)  )
+    lower_cost = np.sum(1e20 * (residuals < 0))
+    cost = upper_cost + lower_cost
+    return cost
+
+
+def guess(x, y):
+    slope, intercept, r_value, p_value, std_err = linregress(x,y)
+    return (intercept, slope)
+
+def underline_regression(x, y, method = "Powell"):
+    start_params = guess(x, y)
+
+    reg = minimize(underline_cost_func,
+               x0 = start_params,
+               args = (x, y),
+               bounds = ((None, None), (0, None)),
+               method = method )
+
+    return (reg.x[0], reg.x[1])
+
+
+def subtract_neuropil_trace_using_regression(f, bg, theta):
+    return f - ( theta[0] + theta[1] * bg)
+
+def get_smoothed_running_minimum(timeseries, tau1 = 30, tau2 = 100):
+    result = minimum_filter1d(uniform_filter1d(timeseries,tau1,mode='nearest'),
+                            tau2,
+                            mode = 'reflect')
+    return result
+
+def get_df_on_f0(F,F0=None):
+    if type(F0)!=type(None):
+        return (F - F0) / F0
+    else:
+        F0 = get_smoothed_running_minimum(F)
+        return get_df_on_f0(F,F0)
+
+
+class UnderlineRegressionFigure:
+    def __init__(self, recording, roi_number, method = 'robust'):
+        sns.set_style("dark")
+        sns.set_context("paper")
+        robust = method in ('robust','seigel','theil sen')
+        f = np.stack((np.zeros(recording.F[roi_number,:].shape),
+                      recording.F[roi_number,:], 
+                      recording.Fneu[roi_number,:]
+                              ),axis=-1)
+        if robust:
+            robust_theta = robust_regression(f[:,2], f[:,1])
+            robust_f = subtract_bg(f[:,1], f[:,2], robust_theta)
+            robust_df_f = get_df_on_f0(robust_f)
+        else:
+            alt_theta = underline_regression(f[:,2], f[:,1],method=method)
+            alt_f = subtract_bg(f[:,1], f[:,2], alt_theta)
+            alt_df_f = get_df_on_f0(alt_f)
+            
+        
+        underline_theta = underline_regression(f[:,2], f[:,1])
+        underline_f = subtract_bg(f[:,1], f[:,2], underline_theta)
+        underline_df_f = get_df_on_f0(underline_f)
+    
+        
+        
+        self.fig = plt.figure(figsize=(8,4),tight_layout=True)
+        
+        regression_axis = self.fig.add_axes((0.075,0.1,0.4,0.85))
+        regression_axis.plot(f[:,2], f[:,1], 'o')
+        if robust:
+            regression_axis.plot(f[:,2], robust_theta[0] + robust_theta[1]*f[:,2], label = 'Theil-Sen Estimator Regression')
+        else:
+            regression_axis.plot(f[:,2], alt_theta[0] + alt_theta[1]*f[:,2], label = f'Underline Regression with {method} method')
+        regression_axis.plot(f[:,2], underline_theta[0] + underline_theta[1]*f[:,2], label = 'Underline Regression with Powell method')
+        regression_axis.set_ylabel('Cell Fluorescence')
+        regression_axis.set_xlabel('Neuropil Fluorescence')
+        regression_axis.legend(loc='upper left')
+        regression_axis.set_yticks(np.fix(np.array(regression_axis.get_ylim())/5)*5)
+        
+        alt_axis = self.fig.add_axes((0.55,0.5,0.4,0.4))
+        if robust:
+            alt_axis.plot(robust_df_f, label = 'Theil-Sen Estimator Regression')
+
+        else:
+            alt_axis.plot(alt_df_f, label = f'Underline Regression with {method} Method')
+        alt_axis.set_ylabel('DF/F')
+        # theilsen_axis.set_xlabel('Sample')
+        alt_axis.set_xticks([])
+        alt_axis.legend(loc='upper right')
+            
+        
+        underline_axis = self.fig.add_axes((0.55,0.1,0.4,0.4))
+        underline_axis.plot(underline_df_f, label = 'Underline Regression')
+        underline_axis.set_ylabel('DF/F')
+        underline_axis.set_xlabel('Sample')
+        underline_axis.set_xticks([])
+        underline_axis.legend(loc='upper right')
+        underline_axis.set_yticks(np.fix(np.array(underline_axis.get_ylim())/5)*5)
+
+
+
+    def show(self):
+        self.fig.show()
--- ProcessFluorescence/df_on_f_novel_regression_test.py
+++ ProcessFluorescence/df_on_f_novel_regression_test.py
@@ -6,42 +6,151 @@ Created on Sun Aug 30 09:59:00 2020
 @author: Vivian Imbriotis
 """
 
+from time import time
+
 import numpy as np
-from scipy.optimize import minimize
-from scipy.stats import linregress
+import seaborn as sns
+from scipy.optimize import minimize, check_grad, approx_fprime
+from scipy.stats import linregress, norm
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.linear_model import TheilSenRegressor
 from scipy.ndimage.filters import minimum_filter1d, uniform_filter1d
 
 from accdatatools.Observations.recordings import Recording
+import pickle as pkl
 
-rec = Recording(path)
 
-f = np.stack((rec.F, rec.Fneu),axis=-1)
+
+
+script_start = time()
+try:
+    with open(r"C:/Users/viviani/desktop/cache.pkl","rb") as file:
+        rec = pkl.load(file)
+except FileNotFoundError:
+    with open(r"C:/Users/viviani/desktop/cache.pkl","wb") as file:
+        rec = Recording(r"H:\Local_Repository\CFEB013\2016-05-31_02_CFEB013")
+        pkl.dump(rec,file)
+
+loading_finished = time()
+
+
 
 
 def underline_cost_func(params, x, y):
     guess = params[0] + params[1] * x
     residuals = y - guess #data points above line are positive
     upper_cost = np.sum(residuals[residuals > 0] / np.sum(residuals > 0)  )
-    lower_cost = np.sum(1e6 * (residuals < 0))
+    lower_cost = np.sum(K * (residuals < 0))
     cost = upper_cost + lower_cost
     return cost
 
+def plot_loss(i):
+    X = np.arange(-200, 300, 12.5)
+    Y = np.arange(-5, 5, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    Z = np.vectorize(lambda x,y:underline_cost_func((x,y),rec.Fneu[i],rec.F[i]))(X,Y)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(X,Y,np.log(Z))
+    ax.set_xlabel("Intercept")
+    ax.set_ylabel("Slope")
+    ax.set_zlabel("Loss")
+    ax.set_title("Loss Function")
+    fig2, ax = plt.subplots(ncols = 2)
+    U,V = np.vectorize(lambda x,y:underline_cost_gradient(
+        (x,y),rec.Fneu[i],rec.F[i],diracs = False))(X,Y)
+    ax[0].quiver(X,Y,U,V, angles = "xy")
+    U,V = np.vectorize(lambda x,y:underline_cost_gradient(
+        (x,y),rec.Fneu[i],rec.F[i],diracs = True))(X,Y)
+    ax[1].quiver(X,Y,U,V, angles = "xy")
+    ax[1].set_title("Gradient of Loss (with approximated diracs)")
+    for a in ax:
+        a.set_xlabel("Intercept")
+        a.set_ylabel("Slope")
+    ax[0].set_title("Gradient of Loss (excluding diracs)")
+    plt.show()
+
+K = 10e10
+    
+def loss(params, x, y):
+    guess = params[0] + params[1] * x
+    residuals = y - guess #data points above line are positive
+    positive_residuals_cost = np.sum(residuals*(residuals>0))
+    negative_residuals_cost = np.sum(-K*residuals*(residuals < 0))
+    negative_slope_cost = -K*params[1]*(params[1]<0)
+    print((positive_residuals_cost,negative_residuals_cost,negative_slope_cost))
+    cost = positive_residuals_cost + negative_residuals_cost + negative_slope_cost
+    return cost
+
+def grad(params, x, y, diracs = False):
+    guess = params[0] + params[1] * x
+    residuals = y - guess #data points above line are positive
+    theta_0 = -np.sum(1.0*(residuals>0) + K*(residuals<0))
+    theta_1 = -np.sum(x*(residuals>0) + x*K*(residuals<0)) + K*(params[0]<0)
+    return (theta_0, theta_1)
+
+
+def plot_clever_loss(i):
+    plt.close('all')
+    X = np.arange(-200, 300, 12.5)
+    Y = np.arange(-5, 5, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    Z = np.vectorize(lambda x,y:loss((x,y),rec.Fneu[i],rec.F[i]))(X,Y)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(X,Y,Z)
+    ax.set_xlabel("Intercept")
+    ax.set_ylabel("Slope")
+    ax.set_zlabel("Loss")
+    ax.set_title("Loss Function")
+    fig2, ax = plt.subplots()
+    U,V = np.vectorize(lambda x,y:grad(
+        (x,y),rec.Fneu[i],rec.F[i],diracs = False))(X,Y)
+    ax.quiver(X,Y,U,V, angles = "xy")
+    ax.set_xlabel("Intercept")
+    ax.set_ylabel("Slope")
+    ax.set_title("-Grad(Loss)")
+    plt.show()
+
+
+dirac = norm(0,1e-2).pdf
+
+
+
+
+def underline_cost_gradient(params, x, y, diracs = False):
+    guess = params[0] + params[1] * x
+    residuals = y - guess #data points above line are positive
+    if diracs:
+        theta_0 = -np.sum((residuals > 0) + K*dirac(residuals))
+        theta_1 = -np.sum(x*[residuals > 0] + x*K*dirac(residuals))
+    else:
+        theta_0 = -np.sum((residuals > 0))
+        theta_1 = -np.sum(x*[residuals > 0])
+    return (theta_0, theta_1)
+
+
 def guess(x, y):
     slope, intercept, r_value, p_value, std_err = linregress(x,y)
-    return [intercept, slope]
+    return (intercept, slope)
 
-def underline_regression(x, y):
+def underline_regression(x, y, method = "Powell"):
     start_params = guess(x, y)
-
-    reg = minimize(underline_cost_func,
-               x0 = start_params,
-               args = (x, y),
-               bounds = ((None, None), (0, None)),
-               method = 'Powell' )
+    if method not in ("cg","CG", "grad desc", "gradient descent"):
+        reg = minimize(underline_cost_func,
+                   x0 = start_params,
+                   args = (x, y),
+                   bounds = ((None, None), (0, None)),
+                   method = method)
+    else:
+        reg = minimize(loss,
+                       x0 = start_params,
+                       jac = grad,
+                       args = (x,y))
 
     return (reg.x[0], reg.x[1])
+
 
 def robust_regression(x, y):
     y = y.reshape(-1, 1)
@@ -67,37 +176,106 @@ def get_df_on_f0(F,F0=None):
         return get_df_on_f0(F,F0)
 
 
+class UnderlineRegressionFigure:
+    color = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    # color[0] = 'blue'
+    # color[1] = 'orange'
+    # color[2] = 'green'
+    # color[3] = 'red'
+    def __init__(self, recording, roi_number, method = 'robust'):
+        sns.set_style("dark")
+        sns.set_context("paper")
+        robust = method in ('robust','seigel','theil sen')
+        f = np.stack((np.zeros(recording.F[roi_number,:].shape),
+                      recording.F[roi_number,:], 
+                      recording.Fneu[roi_number,:]
+                              ),axis=-1)
+        if robust:
+            robust_theta = robust_regression(f[:,2], f[:,1])
+            robust_f = subtract_bg(f[:,1], f[:,2], robust_theta)
+            robust_df_f = get_df_on_f0(robust_f)
+        elif method in ("cg","CG","grad descent", "gradient descent"):
+            alt_theta = underline_regression(f[:,2], f[:,1],method="CG")
+            alt_f = subtract_bg(f[:,1], f[:,2], alt_theta)
+            alt_df_f = get_df_on_f0(alt_f)
+        else:
+            alt_theta = underline_regression(f[:,2], f[:,1],method=method)
+            alt_f = subtract_bg(f[:,1], f[:,2], alt_theta)
+            alt_df_f = get_df_on_f0(alt_f)
+            
+        
+        underline_theta = underline_regression(f[:,2], f[:,1])
+        underline_f = subtract_bg(f[:,1], f[:,2], underline_theta)
+        underline_df_f = get_df_on_f0(underline_f)
+    
+        
+        
+        self.fig = plt.figure(figsize=(8,4),tight_layout=True)
+        
+        regression_axis = self.fig.add_axes((0.075,0.1,0.4,0.85))
+        regression_axis.plot(f[:,2], f[:,1], 'o', color = self.color[0])
+        if robust:
+            regression_axis.plot(f[:,2], robust_theta[0] + robust_theta[1]*f[:,2], label = 'Theil-Sen Estimator Regression',
+                                 color = self.color[1])
+        else:
+            regression_axis.plot(f[:,2], alt_theta[0] + alt_theta[1]*f[:,2], label = f'Underline Regression with {method} method',
+                                 color = self.color[1])
+        regression_axis.plot(f[:,2], underline_theta[0] + underline_theta[1]*f[:,2], label = 'Underline Regression with Powell method',
+                             color = self.color[2])
+        regression_axis.set_ylabel('Cell Fluorescence')
+        regression_axis.set_xlabel('Neuropil Fluorescence')
+        regression_axis.legend(loc='upper left')
 
-robust_theta = robust_regression(f[:,2], f[:,1])
-robust_f = subtract_bg(f[:,1], f[:,2], robust_theta)
-robust_df_f = get_df_on_f0(robust_f)
+        
+        alt_axis = self.fig.add_axes((0.55,0.5,0.4,0.4))
+        if robust:
+            alt_axis.plot(robust_df_f, label = 'Theil-Sen Estimator Regression',
+                          color = self.color[1])
 
-underline_theta = underline_regression(f[:,2], f[:,1])
-underline_f = subtract_bg(f[:,1], f[:,2], underline_theta)
-underline_df_f = get_df_on_f0(underline_f)
+        else:
+            alt_axis.plot(alt_df_f, label = f'Underline Regression with {method} Method',
+                          color = self.color[1])
+        alt_axis.set_ylabel('DF/F')
+        # theilsen_axis.set_xlabel('Sample')
+        alt_axis.set_xticks([])
+        alt_axis.legend(loc='upper right')
+            
+        
+        underline_axis = self.fig.add_axes((0.55,0.1,0.4,0.4))
+        underline_axis.plot(underline_df_f, label = 'Underline Regression with Powell Method',
+                            color=self.color[2])
+        underline_axis.set_ylabel('DF/F')
+        underline_axis.set_xlabel('Sample')
+        underline_axis.set_xticks([])
+        underline_axis.legend(loc='upper right')
+        
+        self.axes = (regression_axis, alt_axis, underline_axis)
+        self.f = f
+    def show(self):
+        self.fig.show()
+        
+class ThreeKindsOfRegressionFigure(UnderlineRegressionFigure):
+    def __init__(self,recording,roi_number):
+        super().__init__(recording,roi_number)
+        (regression_axis, alt_axis, underline_axis) = self.axes
+        f = self.f
+        alt_axis.set_position((0.55,0.37,0.4,0.27),which='original')
+        underline_axis.set_position((0.55,0.1,0.4,0.27),which = 'original')
+        
+        boring_theta = guess(f[:,2], f[:,1])
+        boring_f = subtract_bg(f[:,1], f[:,2], boring_theta)
+        boring_df_f = get_df_on_f0(boring_f)
+        regression_axis.plot(f[:,2], boring_theta[0] + boring_theta[1]*f[:,2], 
+                             label = 'OLS Regression',
+                             color = self.color[3])
+        regression_axis.legend()
+        boring_axis = self.fig.add_axes((0.55,0.64,0.4,0.27))
+        boring_axis.plot(boring_df_f, label = 'OLS Regresison', color = self.color[3])
+        boring_axis.set_ylabel('DF/F')
+        boring_axis.set_xticks([])
+        boring_axis.legend(loc='upper right')
 
 
-plt.figure()
-plt.plot(f[:,2], f[:,1], 'o')
-plt.plot(f[:,2], robust_theta[0] + robust_theta[1]*f[:,2], label = 'Robust Regression')
-plt.plot(f[:,2], underline_theta[0] + underline_theta[1]*f[:,2], label = 'Underline Regression')
-plt.ylabel('Cell Fluorescence')
-plt.xlabel('Neuropil Fluorescence')
-plt.legend(loc='upper left')
-plt.show(block = False)
+plot_clever_loss(6)
+UnderlineRegressionFigure(rec, 4, method="CG").show()
 
-plt.figure()
-plt.subplot(211)
-plt.plot(robust_df_f, label = 'Robust Regression')
-plt.ylabel('DF/F')
-plt.xlabel('Sample')
-plt.legend(loc='upper right')
-
-plt.subplot(212)
-plt.plot(underline_df_f, label = 'Underline Regression')
-plt.ylabel('DF/F')
-plt.xlabel('Sample')
-plt.legend(loc='upper right')
-
-plt.tight_layout(pad=3.0)
-plt.show(block = False)
