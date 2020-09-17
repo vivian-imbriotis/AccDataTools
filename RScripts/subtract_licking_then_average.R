00bdd780a17dc3eb2a3478f7aec03e9dff8167aa
Vivian Imbriotis <viviani@utas.edu.au>
2020-09-17 11:04:09 +1000

    remote

--- RScripts/subtract_licking_then_average.R
+++ RScripts/subtract_licking_then_average.R
@@ -1,4 +1,6 @@
-source_file <- "C:/Users/viviani/Desktop/full_datasets_for_analysis/left_only_high_contrast.csv"
+source_file_left_only <- "C:/Users/viviani/Desktop/full_datasets_for_analysis/left_only_high_contrast.csv"
+source_file_left_and_right <- "C:/Users/viviani/Desktop/full_datasets_for_analysis/both_sides_high_contrast.csv"
+source_file_low_contrast <- "C:/Users/viviani/Desktop/full_datasets_for_analysis/low_contrast.csv"
 
 get_lm_pvalue <- function (modelobject) {
   if (class(modelobject) != "lm") stop("Not an object of class 'lm' ")
@@ -12,8 +14,9 @@ collapse.across.time <- function(dat){
   
   num_trials <- sum(dat$trial_factor==1)
   trials.correct <- dat$correct[dat$trial_factor==1]
-  trials.go <- dat$go[dat$trial_factor==1]
-  trials.side <- dat$side[dat$trial_factor==1]
+  trials.go      <- dat$go[dat$trial_factor==1]
+  trials.side    <- dat$side[dat$trial_factor==1]
+  trials.contrast<- dat$contrast[dat$trial_factor==1]
   result = data.frame(mean.dF = numeric(num_trials*3),
                       trial.segment = character(num_trials*3),
                       stringsAsFactors = FALSE)
@@ -47,18 +50,21 @@ collapse.across.time <- function(dat){
   result$mean.dF[result_idx+2]       <- mean.df.resp
   result$trial.segment[result_idx+2] <- "Response"
   
-  result$correct <- rep(trials.correct,3)
-  result$go      <- rep(trials.go,3)
-  result$side    <- rep(trials.side,3)
+  result$correct <- as.factor(rep(trials.correct,3))
+  result$go      <- as.factor(rep(trials.go,3))
+  result$side    <- as.factor(rep(trials.side,3))
+  result$contrast<- as.factor(rep(trials.contrast,3))
   
   
   result <- na.omit(result)
   result$trial.segment <- as.factor(result$trial.segment)
   return(result)
 }
-
-
-dat <- read.csv(source_file)
+###############################################
+## ANALYSIS OF LEFT_ONLY (MONOCULAR) DATASET ##
+###############################################
+print("Beginning Analysis of Monocular Data...")
+dat <- read.csv(source_file_left_only)
 dat <- dat[!is.na(dat$dF_on_F),]
 rois <- unique(dat$ROI_ID)
 
@@ -68,6 +74,7 @@ licking_insignificant = 0
 summary_objects = vector(mode = "list", length= length(rois))
 model_pvals = numeric(length(rois))
 anovas = vector(mode = "list", length = length(rois))
+
 
 for(i in 1:length(rois)){
   roi <- rois[i]
@@ -108,3 +115,114 @@ anova_frame_pvals <- anova_frame_pvals[,1:3]
 anova_frame_fvals <- anova_frame_fvals[,1:3]
 anova_frame <- cbind(anova_frame_pvals,anova_frame_fvals)
 write.csv(anova_frame,"left_only_collapsed_lm_anova_results.csv")
+
+#######################################################
+## ANALYSIS OF BINOCULAR, HIGH-CONTRAST STIM DATASET ##
+#######################################################
+print("Beginning analysis of binocular high contrast data")
+dat <- read.csv(source_file_left_and_right)
+dat <- dat[!is.na(dat$dF_on_F),]
+rois <- unique(dat$ROI_ID)
+
+licking_model_pvalues = numeric(length(rois))
+licking_significant = 0
+licking_insignificant = 0
+summary_objects = vector(mode = "list", length= length(rois))
+model_pvals = numeric(length(rois))
+anovas = vector(mode = "list", length = length(rois))
+
+for(i in 1:length(rois)){
+  roi <- rois[i]
+  subset <- dat[dat$ROI_ID==roi,]
+  outside_trials  <- subset[subset$trial_factor== -999,]
+  licking.model <- lm(dF_on_F ~ lick_factor, 
+                      data = outside_trials)
+  if(get_lm_pvalue(licking.model)>0.05){
+    licking_insignificant = licking_insignificant + 1
+    licking_model <- lm(dF_on_F ~ 1, data = outside_trials)
+  }
+  licking.prediction <- predict(licking.model, newdata = subset)
+  subset$residuals <- subset$dF_on_F - licking.prediction
+  residual.dat <- subset
+  residual.dat$dF_on_F <- subset$dF_on_F - licking.prediction
+  collapsed.after.licking.subtraction <- collapse.across.time(residual.dat)
+  lm.with.licking.subtraction<- lm(mean.dF ~ trial.segment + trial.segment:correct
+                                   +trial.segment:go + trial.segment:side,
+                                   data = collapsed.after.licking.subtraction)
+  
+  summary_objects[[i]] <- summary(lm.with.licking.subtraction)
+  anovas[[i]] <- anova(lm.with.licking.subtraction)
+  model_pvals[[i]] <- get_lm_pvalue(lm.with.licking.subtraction)
+}
+
+model_pvals     <- p.adjust(model_pvals, method = "fdr")
+rsquareds       <- lapply(summary_objects, function(x) x$adj.r.squared)
+coeffs          <- lapply(summary_objects, function(x) x$coefficients)
+coeff_estimates <- data.frame(do.call(rbind, lapply(coeffs,function(x) x[,"Estimate"])))
+coeff_pvals     <- data.frame(do.call(rbind, lapply(coeffs,function(x) x[,"Pr(>|t|)"])))
+
+anova_frame_pvals <- data.frame(t(rbind(sapply(anovas,FUN=function(x) x$`Pr(>F)`))))
+anova_frame_fvals <- data.frame(t(rbind(sapply(anovas,FUN=function(x) x$`F value`))))
+colnames(anova_frame_pvals) <- colnames(anova_frame_pvals) <- sapply(row.names(anovas[[1]]),FUN=function(x) paste(x,"pvalue",sep="."))
+colnames(anova_frame_fvals) <- colnames(anova_frame_fvals) <- sapply(row.names(anovas[[1]]),FUN=function(x) paste(x,"fvalue",sep="."))
+#Drop the residuals columns from the ANOVA output matrix
+anova_frame_pvals <- anova_frame_pvals[,1:4]
+anova_frame_fvals <- anova_frame_fvals[,1:4]
+anova_frame <- cbind(anova_frame_pvals,anova_frame_fvals)
+write.csv(anova_frame,"both_sides_collapsed_lm_anova_results.csv")
+
+#######################################################
+## ANALYSIS OF BINOCULAR, LOW-CONTRAST STIM DATASET  ##
+#######################################################
+print("Beginning analysis of low-contrast data")
+dat <- read.csv(source_file_low_contrast)
+dat <- dat[!is.na(dat$dF_on_F),]
+rois <- unique(dat$ROI_ID)
+
+licking_model_pvalues = numeric(length(rois))
+licking_significant = 0
+licking_insignificant = 0
+summary_objects = vector(mode = "list", length= length(rois))
+model_pvals = numeric(length(rois))
+anovas = vector(mode = "list", length = length(rois))
+
+for(i in 1:length(rois)){
+  roi <- rois[i]
+  subset <- dat[dat$ROI_ID==roi,]
+  outside_trials  <- subset[subset$trial_factor== -999,]
+  licking.model <- lm(dF_on_F ~ lick_factor, 
+                      data = outside_trials)
+  if(get_lm_pvalue(licking.model)>0.05){
+    licking_insignificant = licking_insignificant + 1
+    licking_model <- lm(dF_on_F ~ 1, data = outside_trials)
+  }
+  licking.prediction <- predict(licking.model, newdata = subset)
+  subset$residuals <- subset$dF_on_F - licking.prediction
+  residual.dat <- subset
+  residual.dat$dF_on_F <- subset$dF_on_F - licking.prediction
+  collapsed.after.licking.subtraction <- collapse.across.time(residual.dat)
+  lm.with.licking.subtraction<- lm(mean.dF ~ trial.segment + trial.segment:correct
+                                   +trial.segment:go + trial.segment:side
+                                   +trial.segment:correct:contrast,
+                                   data = collapsed.after.licking.subtraction)
+  
+  summary_objects[[i]] <- summary(lm.with.licking.subtraction)
+  anovas[[i]] <- anova(lm.with.licking.subtraction)
+  model_pvals[[i]] <- get_lm_pvalue(lm.with.licking.subtraction)
+}
+
+model_pvals     <- p.adjust(model_pvals, method = "fdr")
+rsquareds       <- lapply(summary_objects, function(x) x$adj.r.squared)
+coeffs          <- lapply(summary_objects, function(x) x$coefficients)
+coeff_estimates <- data.frame(do.call(rbind, lapply(coeffs,function(x) x[,"Estimate"])))
+coeff_pvals     <- data.frame(do.call(rbind, lapply(coeffs,function(x) x[,"Pr(>|t|)"])))
+
+anova_frame_pvals <- data.frame(t(rbind(sapply(anovas,FUN=function(x) x$`Pr(>F)`))))
+anova_frame_fvals <- data.frame(t(rbind(sapply(anovas,FUN=function(x) x$`F value`))))
+colnames(anova_frame_pvals) <- colnames(anova_frame_pvals) <- sapply(row.names(anovas[[1]]),FUN=function(x) paste(x,"pvalue",sep="."))
+colnames(anova_frame_fvals) <- colnames(anova_frame_fvals) <- sapply(row.names(anovas[[1]]),FUN=function(x) paste(x,"fvalue",sep="."))
+#Drop the residuals columns from the ANOVA output matrix
+anova_frame_pvals <- anova_frame_pvals[,1:5]
+anova_frame_fvals <- anova_frame_fvals[,1:5]
+anova_frame <- cbind(anova_frame_pvals,anova_frame_fvals)
+write.csv(anova_frame,"both_sides_collapsed_lm_anova_results.csv")
--- ResultsVisualisation/pie_chart.py
+++ ResultsVisualisation/pie_chart.py
@@ -10,7 +10,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import seaborn as sns
+from adjustText import adjust_text
 sns.set_style("darkgrid")
+
+
+readable_titles = {
+    "trial.segment.fvalue" : "During Trial",
+    "trial.segment:correct.fvalue" : "Correct/Incorrect Response",
+    "trial.segment:go.fvalue" : "Go/No-Go Stimulus",
+    "trial.segment:side.fvalue" : "Left/Right Stimulus",
+    "trial.segment:correct:contrast.fvalue" : "High/Low Contrast",
+    "trial.segment.pvalue" : "During Trial",
+    "trial.segment:correct.pvalue" : "Correct/Incorrect Response",
+    "trial.segment:go.pvalue" : "Go/No-Go Stimulus",
+    "trial.segment:side.pvalue" : "Left/Right Stimulus",
+    "trial.segment:correct:contrast.pvalue" : "High/Low Contrast"
+    }
 
 def count_unique_index(df, by):                                                                                                                                                 
     return df.groupby(by).size().reset_index().rename(columns={0:'count'})
@@ -18,12 +33,13 @@ def count_unique_index(df, by):
 df = pd.read_csv("C:/Users/viviani/Documents/left_only_collapsed_lm_anova_results.csv")
 
 class PieChartAnovaFigure:
+    colors = sns.color_palette()
     def __init__(self,df,dataset='left_only'):
-        colors = sns.color_palette()
         pvalue_cols = [c for c in df.columns if 'pvalue' in c]
-        fvalue_cols = [c for c in df.columns if 'fvalue']
+        fvalue_cols = [c for c in df.columns if 'fvalue' in c]
         pvals = df[pvalue_cols]
         fvals = df[fvalue_cols]
+        print(pvalue_cols)
         counter = count_unique_index((pvals<0.05),pvalue_cols)
         counter['percent']=counter['count']/counter['count'].sum()
         if dataset=='left_only':     names = np.array(('Trials','Correct','Go'))
@@ -35,110 +51,114 @@ class PieChartAnovaFigure:
         names = [names[boolrow[:-2].to_numpy().astype('bool')] for _,boolrow in counter.iterrows()]
         names = np.array(['&'.join(ls) if list(ls) else "None" for ls in names])
         self.fig = plt.figure(figsize = [12,5], tight_layout=True)
-        gs = GridSpec(3,2,figure=fig)
-        ax0 = fig.add_subplot(gs[:,0])
-        ax1 = fig.add_subplot(gs[0,1])
-        ax2 = fig.add_subplot(gs[1,1])
-        ax3 = fig.add_subplot(gs[2,1])
-        ax2.set_ylabel("Frequency")
-        ax3.set_xlabel("F value")
+        gs = GridSpec(len(fvals.columns),2,figure=self.fig)
+        right_ax = []
+        left_ax = self.fig.add_subplot(gs[:,0])
+        for i,c in enumerate(fvals.columns):
+            right_ax.append(
+                self.fig.add_subplot(gs[i,1])
+                )
+            right_ax[i].hist(fvals[c][pvals[c.replace('fvalue','pvalue')]<0.05],
+                         label = readable_titles[c],
+                         color = self.colors[i])
+            right_ax[i].legend()
         
-        wedges,text1,text2 = ax0.pie(counter['count'],#labels=names,
+
+        right_ax[2].set_ylabel("Frequency")
+        right_ax[-1].set_xlabel("F value")
+        
+        wedges,text1,text2 = left_ax.pie(counter['count'],#labels=names,
                                 autopct='%1.f%%',counterclock=True,
                                 startangle=60, pctdistance=0.9)
         
-        bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+
         kw = dict(arrowprops=dict(arrowstyle="-",color='black'),
                   zorder=5, va="center")
         
+        annotations = []
         for i, (p,c) in enumerate(zip(wedges,counter['count'])):
             ang = (p.theta2 - p.theta1)/2. + p.theta1
             y = np.sin(np.deg2rad(ang))
             x = np.cos(np.deg2rad(ang))
             horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
-            connectionstyle = "angle,angleA=0,angleB={}".format(ang)
-            kw["arrowprops"].update({"connectionstyle": connectionstyle})
-        
-            ax0.annotate(names[i], xy=(x, y), xytext=(x+np.sign(x)*0.2,
-                                                      y*1.2),
-                         annotation_clip=False,
-                         horizontalalignment=horizontalalignment, **kw)
-        
-        
-        ax0.set_title("Significance of predictors on ROI Fluorescence")
-        ax1.set_title("F values of predictors (when significant)")
-        ax1.hist(fvals["trial.segment.fvalue"][pvals["trial.segment.pvalue"]<0.05],
-                  color = colors[4],label='Trial occuring')
-        ax2.hist(fvals["trial.segment:correct.fvalue"][pvals["trial.segment:correct.pvalue"]<0.05],
-                  color = colors[2], label = 'Go/Nogo trial')
-        ax3.hist(fvals["trial.segment:go.fvalue"][pvals["trial.segment:go.pvalue"]<0.05],
-                  color = colors[1], label = 'Correct/Incorrect trial')
-        xlims = map(lambda a:a.get_xlim(),(ax1,ax2,ax3))
+            annotations.append(
+                left_ax.annotate(names[i], xy=(x, y), 
+                                 xytext=(x+np.sign(x)*0.2,y*1.2),
+                                 annotation_clip=False,
+                                 horizontalalignment=horizontalalignment, 
+                                 **kw)
+                )
+        left_ax.set_title("Significance of predictors on ROI Fluorescence")
+        right_ax[0].set_title("F values of predictors (when significant)")
         new_xlim = (0,None)
-        for a in (ax1,ax2,ax3): a.set_xlim(new_xlim); a.legend(loc='upper right')
+        for a in right_ax: a.set_xlim(new_xlim); a.legend(loc='upper right')
+        adjust_text(annotations)
     def show(self):
-        self.fig.show
+        self.fig.show()
 
-pvals = df[["trial.segment.pvalue",
-            "trial.segment:correct.pvalue",
-            "trial.segment:go.pvalue"]]
-fvals = df[["trial.segment.fvalue",
-            "trial.segment:correct.fvalue",
-            "trial.segment:go.fvalue"]]
+df = pd.read_csv("C:/Users/viviani/Documents/dump.csv")
+PieChartAnovaFigure(df,'left_only').show()
 
-counter = count_unique_index((pvals<0.05),["trial.segment.pvalue",
-                                           "trial.segment:correct.pvalue",
-                                           "trial.segment:go.pvalue"])
-counter['percent']=counter['count']/counter['count'].sum()
+# pvals = df[["trial.segment.pvalue",
+#             "trial.segment:correct.pvalue",
+#             "trial.segment:go.pvalue"]]
+# fvals = df[["trial.segment.fvalue",
+#             "trial.segment:correct.fvalue",
+#             "trial.segment:go.fvalue"]]
 
-names = np.array(('Trials','Correct','Go'))
-names = [names[boolrow[:-2].to_numpy().astype('bool')] for _,boolrow in counter.iterrows()]
-names = ['&'.join(ls) if list(ls) else "None" for ls in names]
-fig = plt.figure(figsize = [12,5], tight_layout=True)
-gs = GridSpec(3,2,figure=fig)
-ax0 = fig.add_subplot(gs[:,0])
-ax1 = fig.add_subplot(gs[0,1])
-ax2 = fig.add_subplot(gs[1,1])
-ax3 = fig.add_subplot(gs[2,1])
-ax2.set_ylabel("Frequency")
-ax3.set_xlabel("F value")
+# counter = count_unique_index((pvals<0.05),["trial.segment.pvalue",
+#                                            "trial.segment:correct.pvalue",
+#                                            "trial.segment:go.pvalue"])
+# counter['percent']=counter['count']/counter['count'].sum()
 
-
-colors = sns.color_palette()
-
-
-
-wedges,text1,text2 = ax0.pie(counter['count'],#labels=names,
-                        autopct='%1.f%%',counterclock=True,
-                        startangle=60, pctdistance=0.9)
-
-bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
-kw = dict(arrowprops=dict(arrowstyle="-",color='black'),
-          zorder=5, va="center")
-
-for i, (p,c) in enumerate(zip(wedges,counter['count'])):
-    ang = (p.theta2 - p.theta1)/2. + p.theta1
-    y = np.sin(np.deg2rad(ang))
-    x = np.cos(np.deg2rad(ang))
-    horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
-    connectionstyle = "angle,angleA=0,angleB={}".format(ang)
-    kw["arrowprops"].update({"connectionstyle": connectionstyle})
-
-    ax0.annotate(names[i], xy=(x, y), xytext=(x+np.sign(x)*0.2,
-                                              y*1.2),
-                 annotation_clip=False,
-                 horizontalalignment=horizontalalignment, **kw)
+# names = np.array(('Trials','Correct','Go'))
+# names = [names[boolrow[:-2].to_numpy().astype('bool')] for _,boolrow in counter.iterrows()]
+# names = ['&'.join(ls) if list(ls) else "None" for ls in names]
+# fig = plt.figure(figsize = [12,5], tight_layout=True)
+# gs = GridSpec(3,2,figure=fig)
+# ax0 = fig.add_subplot(gs[:,0])
+# ax1 = fig.add_subplot(gs[0,1])
+# ax2 = fig.add_subplot(gs[1,1])
+# ax3 = fig.add_subplot(gs[2,1])
+# ax2.set_ylabel("Frequency")
+# ax3.set_xlabel("F value")
 
 
-ax0.set_title("Significance of predictors on ROI Fluorescence")
-ax1.set_title("F values of predictors (when significant)")
-ax1.hist(fvals["trial.segment.fvalue"][pvals["trial.segment.pvalue"]<0.05],
-          color = colors[4],label='Trial occuring')
-ax2.hist(fvals["trial.segment:correct.fvalue"][pvals["trial.segment:correct.pvalue"]<0.05],
-          color = colors[2], label = 'Go/Nogo trial')
-ax3.hist(fvals["trial.segment:go.fvalue"][pvals["trial.segment:go.pvalue"]<0.05],
-          color = colors[1], label = 'Correct/Incorrect trial')
-xlims = map(lambda a:a.get_xlim(),(ax1,ax2,ax3))
-new_xlim = (0,None)
-for a in (ax1,ax2,ax3): a.set_xlim(new_xlim); a.legend(loc='upper right')
-fig.show()
+# colors = sns.color_palette()
+
+
+
+# wedges,text1,text2 = ax0.pie(counter['count'],#labels=names,
+#                         autopct='%1.f%%',counterclock=True,
+#                         startangle=60, pctdistance=0.9)
+
+# bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+# kw = dict(arrowprops=dict(arrowstyle="-",color='black'),
+#           zorder=5, va="center")
+
+# for i, (p,c) in enumerate(zip(wedges,counter['count'])):
+#     ang = (p.theta2 - p.theta1)/2. + p.theta1
+#     y = np.sin(np.deg2rad(ang))
+#     x = np.cos(np.deg2rad(ang))
+#     horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+#     connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+#     kw["arrowprops"].update({"connectionstyle": connectionstyle})
+
+#     ax0.annotate(names[i], xy=(x, y), xytext=(x+np.sign(x)*0.2,
+#                                               y*1.2),
+#                  annotation_clip=False,
+#                  horizontalalignment=horizontalalignment, **kw)
+
+
+# ax0.set_title("Significance of predictors on ROI Fluorescence")
+# ax1.set_title("F values of predictors (when significant)")
+# ax1.hist(fvals["trial.segment.fvalue"][pvals["trial.segment.pvalue"]<0.05],
+#           color = colors[4],label='Trial occuring')
+# ax2.hist(fvals["trial.segment:correct.fvalue"][pvals["trial.segment:correct.pvalue"]<0.05],
+#           color = colors[2], label = 'Correct/Incorrect trial')
+# ax3.hist(fvals["trial.segment:go.fvalue"][pvals["trial.segment:go.pvalue"]<0.05],
+#           color = colors[1], label = 'Go/No-Go trial')
+# xlims = map(lambda a:a.get_xlim(),(ax1,ax2,ax3))
+# new_xlim = (0,None)
+# for a in (ax1,ax2,ax3): a.set_xlim(new_xlim); a.legend(loc='upper right')
+# fig.show()
